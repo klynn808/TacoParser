6e83cc0ba2b2d528a26529e715cbeaf20a28390c
Kate Lynn Thomson <katelynn.jaremko@gmail.com>
2024-10-17 12:40:04 -0500

    exercise finished

--- LoggingKata.Test/TacoParserTests.cs
+++ LoggingKata.Test/TacoParserTests.cs
@@ -16,20 +16,14 @@ namespace LoggingKata.Test
 
             //Assert
             Assert.NotNull(actual);
-
         }
 
         [Theory]
         [InlineData("34.073638, -84.677017, Taco Bell Acwort...", -84.677017)]
-        //Add additional inline data. Refer to your CSV file.
+        [InlineData("32.92496, -85.961342, Taco Bell Alexander Cit...", -85.961342)]
+        [InlineData("34.992219,-86.841402,Taco Bell Ardmore...", -86.841402)]
         public void ShouldParseLongitude(string line, double expected)
         {
-            // TODO: Complete the test with Arrange, Act, Assert steps below.
-            //       Note: "line" string represents input data we will Parse 
-            //       to extract the Longitude.  
-            //       Each "line" from your .csv file
-            //       represents a TacoBell location
-
             //Arrange
             var tacoParserTester = new TacoParser();
 
@@ -40,19 +34,12 @@ namespace LoggingKata.Test
             Assert.Equal(expected, actual);
         }
 
-
-        //TODO: Create a test called ShouldParseLatitude
         [Theory]
-        [InlineData("34.073638, -84.677017, Taco Bell Acwort...", -84.677017)]
-        //Add additional inline data. Refer to your CSV file.
+        [InlineData("34.073638, -84.677017, Taco Bell Acwort...", 34.073638)]
+        [InlineData("30.903723,-84.556037,Taco Bell Bainbridg...", 30.903723)]
+        [InlineData("34.888408,-85.267909,Taco Bell Chickamaug...", 34.888408)]
         public void ShouldParseLatitude(string line, double expected)
         {
-            // TODO: Complete the test with Arrange, Act, Assert steps below.
-            //       Note: "line" string represents input data we will Parse 
-            //       to extract the Longitude.  
-            //       Each "line" from your .csv file
-            //       represents a TacoBell location
-
             //Arrange
             var tacoParserTester = new TacoParser();
 
--- LoggingKata/Program.cs
+++ LoggingKata/Program.cs
@@ -14,13 +14,8 @@ namespace LoggingKata
 
         static void Main(string[] args)
         {
-            // Objective: Find the two Taco Bells that are the farthest apart from one another.
-            // Some of the TODO's are done for you to get you started. 
-
             logger.LogInfo("Log initialized.......");
 
-            // Use File.ReadAllLines(path) to grab all the lines from your csv file. DONE
-            // Optional: Log an error if you get 0 lines and a warning if you get 1 line DONE
             var lines = File.ReadAllLines(csvPath);
             if (lines.Length == 0)
             {
@@ -31,49 +26,23 @@ namespace LoggingKata
                 logger.LogWarning("not enough input");
             }
 
-            // This will display the first item in your lines array
             logger.LogInfo($"Lines: {lines[0]}");
 
-            // Create a new instance of your TacoParser class
             var parser = new TacoParser();
 
-            // Use the Select LINQ method to parse every line in lines collection
             ITrackable[] locations = lines.Select(line => parser.Parse(line)).ToArray();
 
-            // Complete the Parse method in TacoParser class first and then START BELOW ---------- DONE
-
-            // TODO: Create two `ITrackable` variables with initial values of `null`. 
-            // These will be used to store your two Taco Bells that are the farthest from each other. DONE
-            // TODO: Create a `double` variable to store the distance DONE
             ITrackable furthestTacoBellOne = null;
             ITrackable furthestTacoBellTwo = null;
             double distance = 0;
 
-            // TODO: Add the Geolocation library to enable location comparisons: using GeoCoordinatePortable;
-            // Look up what methods you have access to within this library. DONE
-
-            // NESTED LOOPS SECTION----------------------------
-
-            // FIRST FOR LOOP -
-            // TODO: Create a loop to go through each item in your collection of locations.
-            // This loop will let you select one location at a time to act as the "starting point" or "origin" location.
-            // Naming suggestion for variable: `locA`
-            // TODO: Once you have locA, create a new Coordinate object called `corA` with your locA's latitude and longitude. DONE
             for (int i = 0; i < locations.Length; i++)
             {
                 var locA = locations[i];
-                //locA is what location we are on
-                //i goes from 0 to 237
                 var coordinateA = new GeoCoordinate();
                 coordinateA.Latitude = locA.Location.Latitude;
                 coordinateA.Longitude = locA.Location.Longitude;
 
-                // SECOND FOR LOOP -
-                // TODO: Now, Inside the scope of your first loop, create another loop to iterate through locations again.
-                // This allows you to pick a "destination" location for each "origin" location from the first loop. 
-                // Naming suggestion for variable: `locB`
-
-                // TODO: Once you have locB, create a new Coordinate object called `corB` with your locB's latitude and longitude.
                 for (int j = 0; j < locations.Length; j++)
                 {
                     var locB = locations[j];
@@ -90,18 +59,7 @@ namespace LoggingKata
                 }
             }
 
-            // TODO: Now, still being inside the scope of the second for loop, compare the two locations using `.GetDistanceTo()` method, which returns a double.
-            // If the distance is greater than the currently saved distance, update the distance variable and the two `ITrackable` variables you set above.
-
-            // NESTED LOOPS SECTION COMPLETE ---------------------
-
-            // Once you've looped through everything, you've found the two Taco Bells farthest away from each other.
-            // Display these two Taco Bell locations to the console. DONE
             logger.LogWarning($"{furthestTacoBellOne.Name} and {furthestTacoBellTwo.Name} are the furthest apart.");
-
-
-
-
         }
     }
 }
